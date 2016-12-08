e301f8d1521bd33b0443c31b29ac4d08a64c266f
Vinícius Giles <viniciusgcp@gmail.com>
2016-12-07 23:32:49 -0300

    Update controle.py

--- controle.py
+++ controle.py
@@ -1,90 +1,208 @@
+import time
 from palavra import *
 from lista import Lista
 
 class Controle:
     def __init__(self):
         self.numeroTermos = 0
-        self.termos = list()
         self.dadosCarregados = True
+        self.tempoBusca = float()
+    
+    def __apagarTermos(self):
+        pass
+
+    def carregarDados(self,filename):
+        pass
+
+    def getTempoBusca(self):
+        pass
+
+    def find(self, prefixo, qtd):
+        pass
+
+class Trie(Controle):
+    class Node:
+        def __init__(self, char=None, weight=None):
+            self.char, self.weight = char, weight
+            self.left, self.down, self.right = None, None, None
+            
+        def __str__(self):
+            return self.char
+
+        def __repr__(self):
+            return self.__str__()
+
+    def __init__(self):
+        Controle.__init__(self)
+        self.root = None
+
+    def __apagarTermos(self):
+        self.root = None
+        self.numeroTermos = 0
+        self.dadosCarregados = False
+
+    def getTempoBusca(self):
+        return "%.5f" % self.tempoBusca
+
+    def insert(self,word,weight,node):
+        '''Insere nova string na trie.'''
+        if not node:
+            if not self.root:
+                self.root = node = Trie.Node(word[0])
+            else: node = Trie.Node(word[0])
+
+        if word[0] > node.char:
+            node.right = self.insert(word,weight,node.right)
+        elif word[0] < node.char:
+            node.left = self.insert(word,weight,node.left)
+        else:
+            if len(word) > 1:
+                node.down = self.insert(word[1:],weight,node.down)
+            else:
+                node.weight = weight
+        return node
+
+    def __getByPrefix(self, prefix, node, v=[], skipFirst=True):
+        ''' Retorna um vetor de Palavras com dado prefixo. '''
+        if node is None:
+            return []
+
+        if node.down != None:
+            if node.down.weight != None:
+                v += [Palavra(prefix+node.down.char, node.down.weight)]
+            self.__getByPrefix(prefix+node.down.char, node.down, v, False)
+        if not skipFirst:
+            if node.left != None:
+                if node.left.weight != None:
+                    v += [Palavra(prefix[:-1]+node.left.char, node.left.weight)]
+                self.__getByPrefix(prefix[:-1]+node.left.char, node.left, v, False)
+
+            if node.right != None:
+                if node.right.weight != None:
+                    v += [Palavra(prefix[:-1]+node.right.char, node.right.weight)]
+                self.__getByPrefix(prefix[:-1]+node.right.char, node.right, v, False)
+        return v
+
+    def search(self,prefix):
+        ''' Procura pelo prefixo na trie. '''
+        atual = pai = self.root
+        i = 0
+        while atual is not None and i < len(prefix): # nao None
+            if prefix[i] == atual.char:
+                if i == len(prefix)-1:
+                    if atual.weight != None: # caso o prefixo seja a palavra inteira
+                        return [Palavra(prefix, atual.weight)] + self.__getByPrefix(prefix, atual, [])
+                    else: return self.__getByPrefix(prefix, atual, [])
+                atual = atual.down
+                i+=1
+            elif prefix[i] > atual.char:
+                atual = atual.right
+            else: atual = atual.left
+        return
+
+    def carregarDados(self,filename):
+        if self.dadosCarregados:
+            self.__apagarTermos()
+
+        file = open(filename, 'r')
+        for line in file.readlines()[1:]:
+            dados = line.split()
+            # somente se a linha não for vazia
+            if dados != []:
+                self.insert(' '.join(dados[1:]), int(dados[0]), self.root)
+                self.numeroTermos += 1
+
+        self.dadosCarregados = True
+        file.close()
+
+    def find(self, prefixo, qtd):
+        t1 = time.time()
+        resultados = self.search(prefixo)
+        if resultados is None:
+            return 'Nenhum resultado'
+        sugestoes = Lista()
+        for item in resultados:
+            sugestoes.inserirOrdenado(item, comparaPorPeso)
+            #se a lista de sugestoes passar a qtd, remove ultimo
+            if sugestoes.size() > qtd:
+                sugestoes.removerFim()
+        t2 = time.time()
+        self.tempoBusca = t2 - t1
+        return sugestoes
+
+class BSearch(Controle):
+    def __init__(self):
+        Controle.__init__(self)
+        self.termos = list()
     
     def __apagarTermos(self):
         self.termos = []
         self.numeroTermos = 0
         self.dadosCarregados = False
-    
-    #Pronto
-    def __firstIndexOf(self, prefixo):
-        inicio = 0
-        fim = self.numeroTermos-1                       
-        pos = -1
-        while inicio < fim:
-            pos = (inicio + fim)//2
-            if comparaPorPrefixo(self.termos[pos], prefixo) == 0:
-                fim = pos
-            elif comparaPorPrefixo(self.termos[pos], prefixo) == 1:
-                fim = pos-1
-            else:
-                inicio=pos+1
-        return pos
-    
-    #Pronto
-    def __lastIndexOf(self, prefixo):
-        inicio = 0
-        fim = self.numeroTermos-1                       
-        pos = (inicio + fim)//2
-        while inicio <= fim:
-            print("inicio: {0}\nmeio: {1}\nfim: {2}".format(inicio, pos, fim))
-            if comparaPorPrefixo(self.termos[pos], prefixo) == 0 :
-                if pos == fim:
-                    return pos
-                    break
-                elif comparaPorPrefixo(self.termos[pos+1], prefixo) == 1 :
-                    return pos
-                    break
-                else:
-                    pos = pos +1
-            elif comparaPorPrefixo(self.termos[pos], prefixo) == 1:
-                fim = pos
-                pos = (inicio + fim)//2
-            elif comparaPorPrefixo(self.termos[pos], prefixo) == -1:
-                inicio = pos
-                pos = (inicio + fim)//2
 
-    #Pronto 
+    def getTempoBusca(self):
+        return "%.5f" % self.tempoBusca
+    
+    def __firstIndexOf(self, prefixo):
+        ''' Retorna o indice da primeira ocorrencia
+            do prefixo no vetor de termos. '''
+        inicio = 0
+        fim = self.numeroTermos - 1
+        while fim > inicio:
+            meio = (inicio + fim) // 2
+            cmp = comparaPorPrefixo(self.termos[meio], prefixo)
+            if cmp > 0: # a palavra do meio vem depois do prefixo
+                fim = meio - 1
+            elif cmp < 0: # a palavra do meio vem antes do prefixo
+                inicio = meio + 1
+            else: # a palavra contem o prefixo
+                fim = meio
+        return inicio
+
+    def __lastIndexOf(self, prefixo):
+        ''' Retorna o indice da ultima ocorrencia
+            do prefixo no vetor de termos. '''
+        inicio = 0
+        fim = self.numeroTermos - 1
+        while fim >= inicio:
+            meio = (inicio + fim) // 2
+            if comparaPorPrefixo(self.termos[meio], prefixo) > 0: # a palavra do meio vem depois do prefixo
+                fim = meio - 1
+            else: # a palavra do meio vem antes ou contem o prefixo
+                inicio = meio + 1
+        return fim
+
     def carregarDados(self,filename):
         if self.dadosCarregados:
             self.__apagarTermos()
 
-        f = open(filename, 'r', encoding="utf-8")
-        for linha in f.readlines():
-            termo = linha.split()
-            cpeso = int(termo.pop(0))
+        file = open(filename, 'r')
+        for line in file.readlines()[1:]:
+            dados = line.split()
+            # somente se a linha não for vazia
+            if dados != []:
+                self.termos.append(Palavra(' '.join(dados[1:]), int(dados[0])))
+                self.numeroTermos += 1
 
-            if len(termo) != 0:
-                aux = " ".join(termo)
-                self.termos.append(Palavra(aux,cpeso))
-                self.numeroTermos += 1 
+        self.termos.sort()
+        self.dadosCarregados = True
+        file.close()
 
-        f.close()
-        self.termos.sort() 
-        self.dadosCarregados = True        
-        
     def find(self, prefixo, qtd):
-        first = self.__firstIndexOf(prefixo)
-        last = self.__lastIndexOf(prefixo)
-        termos =  self.termos[first:last]
+        t1 = time.time()
+        f, l = self.__firstIndexOf(prefixo), self.__lastIndexOf(prefixo)
+        # print("{0}: indice {1} a {2}".format(prefixo,f,l))
 
-        selecionadas = Lista()
-        for item in termos:         
-            selecionadas.inserirOrdenado(item, comparaPorPeso)  
-            if selecionadas.size() == qtd:
-                selecionadas.removerFim()
-                
+        # quando f > l ou f == l (e nao achou nada) mas nao ha prefixo
+        if (f > l) or (f == l and comparaPorPrefixo(self.termos[f], prefixo) != 0):
+            return 'Nenhum resultado'
 
-        return selecionadas
-
-    def fl(self, prefixo):
-        return self.__firstIndexOf(prefixo)
-
-a = Controle()
-a.carregarDados('cities.txt')
+        sugestoes = Lista()
+        for item in self.termos[f:l+1]:
+            sugestoes.inserirOrdenado(item, comparaPorPeso)
+            #se a lista de sugestoes passar a qtd, remove ultimo
+            if sugestoes.size() > qtd:
+                sugestoes.removerFim()
+        t2 = time.time()
+        self.tempoBusca = t2-t1
+        return sugestoes
