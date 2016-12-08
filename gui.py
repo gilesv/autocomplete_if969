10eefa103841b36eb53a46ac9ee3226f9c95538f
Vinícius Giles <viniciusgcp@gmail.com>
2016-12-07 23:33:28 -0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,9 +1,9 @@
 from mainwindow import Ui_MainWindow
-from controle import Controle
+from controle import *
 
 class GUI(Ui_MainWindow):
     def __init__(self):
-        self.controle = Controle()
+        self.controle = Trie()
 
     def setupUi(self, MainWindow):
         Ui_MainWindow.setupUi(self, MainWindow)
@@ -11,9 +11,17 @@ class GUI(Ui_MainWindow):
         self.botaoOK.clicked.connect(self.carregarDados)
         self.qtdPalavraText.textChanged.connect(self.autocompletar)
         self.palavraText.textChanged.connect(self.autocompletar)
+        self.radioTrie.toggled.connect(self.useTrie)
+        self.radioBin.toggled.connect(self.useBin)
 
     def inputFileChanged(self):
         self.botaoOK.setEnabled(self.inputFileText.text().strip() != "")
+
+    def useTrie(self):
+    	self.controle = Trie()
+
+    def useBin(self):
+    	self.controle = BSearch()
     
     def carregarDados(self):
         self.controle.carregarDados(self.inputFileText.text())
@@ -24,6 +32,7 @@ class GUI(Ui_MainWindow):
         qtd = self.qtdPalavraText.text()
         if texto != "" and qtd != "":
             lista = self.controle.find(texto,int(qtd))
-            self.outputText.setText(str(lista))            
+            self.outputText.setText(str(lista))
+            self.tempoLabel.setText("Tempo de busca: {0} segundos".format(self.controle.getTempoBusca()))
         else:
             self.outputText.clear()
